c2ff1048615a9b9d5b42ca439b25aa18b58072aa
T-N9 <tenyainmoelwin@gmail.com>
2021-07-18 14:35:11 +0630

    logic fixed

--- scroll-effect/se-1/app.js
+++ scroll-effect/se-1/app.js
@@ -1,16 +1,18 @@
-let container_y= document.getElementById('container-scroll-y');
-let container_x= document.getElementById('container-scroll-x');
+
 
 document.getElementById('switch-btn').onclick=function(){
+    let container_y= document.getElementById('container-scroll-y');
+    let container_x= document.getElementById('container-scroll-x');
+
     container_x.classList.toggle('d-none');
     container_y.classList.toggle('d-none');
 
     if(container_x.classList.contains('d-none')){
-        this.innerText="Scroll Y";
-        console.log('Scroll Y');
+        this.innerText="Scrolling Y";
+        console.log('Scrolling Y');
     }
     else{
-        this.innerText="Scroll X";
-        console.log('Scroll X');
+        this.innerText="Scrolling X";
+        console.log('Scrolling X');
     }
 }
